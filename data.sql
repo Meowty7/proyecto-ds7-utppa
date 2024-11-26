a974fe3c35ff5ea7a81c1c5207bee5617a3195d3
Meowty7 <beltazarw@gmail.com>
2024-11-26 15:46:26 -0500

    scripts sql on run

--- data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `usuarios` (`idUsuario`, `usuario`, `apellido`, `contrasena`, `correo`, `cedula`, `activo`) VALUES
-                                                                                                            (1, 'NaomiADM', 'Perea', '$2y$10$Kw/pfRfddEC93tbBxnYb2.3ntHtmfZdP8vlTZsfh1pxsH6Ynd9i9i', 'naomiperea@gmail.com', '8-789-1456', 0),
-                                                                                                            (2, 'MulinoADM', 'Martinez', '$2y$10$SxgEikDf4PqP6LWLNRjL7.lFXuVsDJ6Uco6kMm6a0FSCES35xZnN2', 'mulinoadm@gmail.com', '4-968-1582', 0);
--- db-init-scripts/01-init1.sql
+++ db-init-scripts/01-init1.sql
@@ -1,15 +1,15 @@
 create table if not exists secciones
 (
     idSeccion   int auto_increment
-    primary key,
+        primary key,
     nombre      varchar(50)  not null,
     descripcion varchar(200) not null
-    );
+);
 
 create table if not exists inventario
 (
     idInventario int auto_increment
-    primary key,
+        primary key,
     parte        varchar(100)   not null,
     marca        varchar(50)    not null,
     modelo       varchar(30)    not null,
@@ -19,8 +19,8 @@ create table if not exists inventario
     idSeccion    int            not null,
     imagen       blob           null,
     constraint inventario_ibfk_1
-    foreign key (idSeccion) references secciones (idSeccion)
-    );
+        foreign key (idSeccion) references secciones (idSeccion)
+);
 
 create index idSeccion
     on inventario (idSeccion);
@@ -28,14 +28,14 @@ create index idSeccion
 create table if not exists movimientos_inventario
 (
     idMovimiento    int auto_increment
-    primary key,
+        primary key,
     idInventario    int                        not null,
     tipoMovimiento  enum ('entrada', 'salida') not null,
     cantidad        int                        not null,
     fechaMovimiento datetime                   not null,
     constraint movimientos_inventario_ibfk_1
-    foreign key (idInventario) references inventario (idInventario)
-    );
+        foreign key (idInventario) references inventario (idInventario)
+);
 
 create index idInventario
     on movimientos_inventario (idInventario);
@@ -43,11 +43,11 @@ create index idInventario
 create table if not exists usuarios
 (
     idUsuario  int auto_increment
-    primary key,
+        primary key,
     usuario    varchar(50)          not null,
     apellido   varchar(50)          not null,
     contrasena varchar(200)         not null,
     correo     varchar(100)         not null,
     cedula     varchar(100),
     activo     tinyint(1) default 1 not null
-    );
+);
