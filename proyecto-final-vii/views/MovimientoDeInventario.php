7d1574c8dfe991f73be7cee0f817a4ac79524929
JosueMorales18 <mrmoralesk16@gmail.com>
2024-11-27 09:46:06 -0500

    fondo

--- proyecto-final-vii/views/MovimientoDeInventario.php
+++ proyecto-final-vii/views/MovimientoDeInventario.php
@@ -106,6 +106,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movimiento de Inventario</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        body {
+            text-align: center;
+            font-family: Arial, sans-serif;
+
+            background-image: url('https://wallpaper.dog/large/20624419.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+    </style>
 </head>
 <body class="bg-gray-100 p-5">
 <div class="container mx-auto">
